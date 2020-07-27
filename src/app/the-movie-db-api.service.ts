fccdbc2b9aa7307bd51fb28b7533e30979378a8c
J.Nicewander <jnicewander@live.com>
2020-07-27 10:11:29 -0400

    Replaced hard coded url string with function to build the url string.

--- src/app/the-movie-db-api.service.ts
+++ src/app/the-movie-db-api.service.ts
@@ -8,11 +8,19 @@ import { HttpClient } from '@angular/common/http'
 export class TheMovieDbApiService {
 
   constructor(private http: HttpClient) { }
-  API_KEY: string = 'a54ab344ecaa235e031650493214996a';
+  baseURL: string = 'https://api.themoviedb.org/3/discover/movie?api_key=a54ab344ecaa235e031650493214996a';
+  initialParameters: string = '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false';
 
-  //  https://api.themoviedb.org/3/search/movie?api_key=a54ab344ecaa235e031650493214996a&language=en-US&query=matrix&page=1&include_adult=false
-  getMovies(){
-    return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=a54ab344ecaa235e031650493214996a&language=en-US&query=matrix&page=1&include_adult=false');
+  getMovies(year?: number) {
+    // api call to populate movies
+    let url: string = `${this.baseURL}`
+    if(year) {
+      url = `${url}${this.initialParameters}&primary_release_year=${year}`;
+    } else {
+      url = `${url}${this.initialParameters}`;
+    }
+
+    return this.http.get(url);
   } 
-  
+
 }
