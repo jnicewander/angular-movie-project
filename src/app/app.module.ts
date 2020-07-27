5df7c85fa6a7190e09273082a37ce2ff744f38fc
J.Nicewander <jnicewander@live.com>
2020-07-27 10:10:24 -0400

    Added FormsModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { TheMovieDbApiService } from './the-movie-db-api.service'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [TheMovieDbApiService],
   bootstrap: [AppComponent]
